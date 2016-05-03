ab1ed50b73ae3bb8c7c6b1d4de6f748519b00d30
Jack McNicol <jackmcpickle@gmail.com>
2016-05-03 11:25:33 +0930

    fix webmock 2.0 and extract helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../app/models', __FILE__)
 
-require 'vcr'
+require 'support/vcr_loader'
 require 'support/coverage_loader'
-
 require 'feefo'
-
-VCR.configure do |c|
-  c.cassette_library_dir = 'spec/cassettes'
-  c.hook_into :webmock
-end
