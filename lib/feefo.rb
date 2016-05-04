95c9cf725609383118b39433ca687d9348c55bde
Jack McNicol <jackmcpickle@gmail.com>
2016-05-04 13:39:32 +0930

    add review_badge_image method

--- lib/feefo.rb
+++ lib/feefo.rb
@@ -20,17 +20,37 @@ module Feefo
   end
 
   def self.review_base_url
-    name = URI.encode(config.fetch(:name))
-    logon = URI.encode(config.fetch(:logon))
-    division = URI.encode(config.fetch(:division))
+    name     = configEncode(:name)
+    logon    = configEncode(:logon)
+    division = configEncode(:division)
     "https://www.feefo.com/reviews/#{name}/?logon=#{logon}/#{division}"
   end
 
-  def self.review_for_code(code)
-    fetcher = ReviewFetcher.new(code)
+  def self.review_badge_image(filters)
+    logon          = configEncode(:logon)
+    division       = configEncode(:division)
+    code           = filters[:code]
+    category       = filters[:category]
+    base_image_url = "https://www.feefo.com/feefo/feefologo.jsp?logon=#{logon}/#{division}"
+    category_part  = category ? category : ''
+    code_part      = code     ? code     : ''
+    image_template = "&template=product-stars-grey-150x38_en.png"
+    return '' unless category || code
+    base_image_url + category_part + code_part + image_template
+  end
+
+  def self.review_for_code(filters)
+    fetcher = ReviewFetcher.new(filters)
     data = JSON.parse(fetcher.fetch_reviews_json)
     Feefo::Reviews.new(data)
   rescue JSON::ParserError => e
     nil
   end
+
+  private
+
+  def configEncode(key)
+    URI.encode(config.fetch(key))
+  end
+
 end
